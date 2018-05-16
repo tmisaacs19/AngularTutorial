b95c685fcd709d36e9f97879a4991e6aa9207087
tmisaacs19 <tmisaacs19@gmail.com>
2018-05-16 10:57:42 -0500

    UpdatedRoutingURL

--- Code/app.js
+++ Code/app.js
@@ -1,5 +1,16 @@
-var myApp = angular.module('myFirstApp', []);
-myApp.factory('personService',  function(){
+angular.module('myFirstApp', ['ngRoute']);
+.config(['$routeProvider', '$locationProvider', function($routeProvider $locationProvider){
+	$routeProvider.when('/about', {
+		templateUrl:'views/about.html'
+	})
+	.when('/contact', {
+		templateUrl:'views/contact.html'
+	})
+	.otherwise({templateUrl: 'views/404.html'})
+
+	$locationProvider.html5Mode(true);
+}])
+.factory('personService',  function(){
 
 	 var person = {};
 
